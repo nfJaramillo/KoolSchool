70bb604d4d3295c42d69b6809d8e9060b730a202
nfJaramillo <nf.jaramillo@uniandes.edu.co>
2019-10-07 19:26:44 -0500

    minnnor fix

--- lib/utils/pregunta.dart
+++ lib/utils/pregunta.dart
@@ -14,7 +14,7 @@ class Pregunta{
   //----------------------------------------------
 
   /// Constructor que recibe una pregunta de tipo String y un boolean sobre si la respuesta es corrrecta o no
-  Pregunta(pPregunta,pRespuesta)
+  Pregunta(pPregunta, pRespuesta)
   {
     pregunta = pPregunta;
     respuesta = pRespuesta;
