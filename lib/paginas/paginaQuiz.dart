eb48ef811538e97966b2c3a05009742ad4b1cdbb
nfJaramillo <nf.jaramillo@uniandes.edu.co>
2019-10-09 16:50:33 -0500

    Mejora preguntas

--- lib/paginas/paginaQuiz.dart
+++ lib/paginas/paginaQuiz.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/UI/botonUI.dart';
+import 'package:hello_world/UI/overlayCorreroIncorrecto.dart';
 import 'package:hello_world/UI/preguntaUI.dart';
+import 'package:hello_world/utils/pregunta.dart';
+import 'package:hello_world/utils/quiz.dart';
 
 class PaginaQuiz extends StatefulWidget {
   @override
@@ -8,17 +11,75 @@ class PaginaQuiz extends StatefulWidget {
 }
 
 class PaginaEstadoQuiz extends State<PaginaQuiz> {
+
+  //----------------------------------------------
+  // ATRIBUTOS
+  //----------------------------------------------
+
+  Pregunta preguntaActual;
+
+  Quiz quiz = new Quiz([
+    new Pregunta("KoolSchool es cool?", true),
+    new Pregunta("Nuestro grupo de PMC tiene 7 personas?", false),
+    new Pregunta("El cielo es azul?", true)]);
+  
+
+  String textoDeLaPregunta;
+
+  int numeroDePreguntaActual;
+
+  bool esCorrecto;
+
+  bool overlayVisible = false;
+
+  //----------------------------------------------
+  // METODOS
+  //----------------------------------------------
+
+  @override
+  void initState() {
+    super.initState();
+    preguntaActual = quiz.siguientePregunta;
+    textoDeLaPregunta = preguntaActual.pregunta;
+    numeroDePreguntaActual = quiz.darNumeroDePreguntaActual;
+  }
+
+  void manejarRespuesta(bool respuesta){
+    esCorrecto = (preguntaActual.respuesta == respuesta);
+    quiz.ganarPunto(esCorrecto);
+    this.setState((){
+      overlayVisible = true;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Stack(
       children: <Widget>[
         new Column(
           children: <Widget>[
-           new BotonUI(Colors.greenAccent, new Text("Verdadero",style: TextStyle(fontSize: 50)),() => null), // Boton verdadero
-           new PreguntaUI(new Text("Pregunta")), // Pregunta
-           new BotonUI(Colors.redAccent, new Text("Falso",style: TextStyle(fontSize: 50)),() => null), // Boton falso
+           new BotonUI(Colors.greenAccent, new Text("Verdadero",style: TextStyle(fontSize: 50)),() => manejarRespuesta(true)), // Boton verdadero
+           new PreguntaUI(new Text(textoDeLaPregunta)), // Pregunta
+           new BotonUI(Colors.redAccent, new Text("Falso",style: TextStyle(fontSize: 50)),() => manejarRespuesta(false)), // Boton falso
           ],
-        )
+        ),
+        overlayVisible == true ? new OverlayCorreroIncorrecto(
+          esCorrecto, 
+          () {
+             
+            if(quiz.darCantidadDePreguntas == numeroDePreguntaActual){
+              null;
+            }
+           
+            preguntaActual = quiz.siguientePregunta;
+            this.setState((){
+              print(preguntaActual.pregunta);
+              overlayVisible = false;
+              textoDeLaPregunta = preguntaActual.pregunta;
+              numeroDePreguntaActual = quiz.darNumeroDePreguntaActual;
+            });
+          }): new Container()
       ],
     );
   }
