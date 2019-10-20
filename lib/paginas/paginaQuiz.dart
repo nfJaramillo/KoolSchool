c4653d16e369057aec0e5ccc3243520de551ba3f
nfJaramillo <nf.jaramillo@uniandes.edu.co>
2019-10-20 10:15:26 -0500

    Eleccion multiple lista

--- lib/paginas/paginaQuiz.dart
+++ lib/paginas/paginaQuiz.dart
@@ -22,13 +22,13 @@ class PaginaEstadoQuiz extends State<PaginaQuiz> {
 
   /// Atributo que corre un nuevo quiz con ciertas preguntas
   Quiz quiz = new Quiz([
-    new Pregunta("¿Miguel es un niño Indígena?", true),
-    new Pregunta("¿Para llegar a Bogotá se demoran 10 días?", false),
-    new Pregunta("¿En la Pedrera deben tomar un avión?", true),
-    new Pregunta("¿El miercoles la gente del pueblo se reune para ver la llegada y la salida del avión?", false),
-    new Pregunta("¿El avión de la Pedrera a Leticia se demora 10 horas?", false),
-    new Pregunta("¿Para viajar de la ciudad de Leticia hacia Bogotá deberan tomar otro avión?", true),
-    new Pregunta("¿Miguel, su papá y su tío pasan la noche en un hotel pequeño?", true)]);
+    new Pregunta.vofYabierta("¿Miguel es un niño Indígena?", "Verdadero", TipoDePregunta.vOF),
+    new Pregunta.multiple("¿Cunato de demoran en llegar Bogotá?", "a", TipoDePregunta.multiple, ["10 dias","1 hora","2 años","10 meses"]),
+    new Pregunta.vofYabierta("¿En la Pedrera deben tomar un avión?", "Verdadero", TipoDePregunta.vOF),
+    new Pregunta.vofYabierta("¿El miercoles la gente del pueblo se reune para ver la llegada y la salida del avión?", "Falso", TipoDePregunta.vOF),
+    new Pregunta.vofYabierta("¿El avión de la Pedrera a Leticia se demora 10 horas?", "Falso", TipoDePregunta.vOF),
+    new Pregunta.vofYabierta("¿Para viajar de la ciudad de Leticia hacia Bogotá deberan tomar otro avión?", "Verdadero", TipoDePregunta.vOF),
+    new Pregunta.vofYabierta("¿Miguel, su papá y su tío pasan la noche en un hotel pequeño?", "Verdadero", TipoDePregunta.vOF)]);
   
 
   /// Atributo con el texto dela pregunta actual  z
@@ -57,7 +57,7 @@ class PaginaEstadoQuiz extends State<PaginaQuiz> {
   }
 
   /// Metodo que se corre luego de que el usuario responde, este comprueba si la respuesta fue correcta
-  void manejarRespuesta(bool respuesta){
+  void manejarRespuesta(String respuesta){
     esCorrecto = (_preguntaActual.darRespuesta == respuesta);
     quiz.ganarPunto(esCorrecto);
     this.setState((){
@@ -70,13 +70,41 @@ class PaginaEstadoQuiz extends State<PaginaQuiz> {
   Widget build(BuildContext context) {
     return new Stack(
       children: <Widget>[
-        new Column(
+
+        if(_preguntaActual.darTipoDePregunta ==  TipoDePregunta.vOF) // Si es pregunta de verdadero o falso contruye esa parte grafica
+          new Column(
           children: <Widget>[
-           new BotonUI(Colors.greenAccent, new Text("Verdadero",style: TextStyle(fontSize: 50)),() => manejarRespuesta(true)), // Boton verdadero
+           new BotonUI(Colors.greenAccent, new Text("Verdadero",style: TextStyle(fontSize: 50)),() => manejarRespuesta("Verdadero")), // Boton verdadero
            new PreguntaUI(new Text(textoDeLaPregunta)), // Pregunta
-           new BotonUI(Colors.redAccent, new Text("Falso",style: TextStyle(fontSize: 50)),() => manejarRespuesta(false)), // Boton falso
+           new BotonUI(Colors.redAccent, new Text("Falso",style: TextStyle(fontSize: 50)),() => manejarRespuesta("Falso")), // Boton falso
           ],
         ),
+
+        if(_preguntaActual.darTipoDePregunta == TipoDePregunta.multiple)
+          new Column(
+          children: <Widget>[
+            Expanded(
+              child: new Row(
+                 children: <Widget>[
+                new BotonUI(Colors.amber, new Text(_preguntaActual.darOpciones[0],style: TextStyle(fontSize: 50)),() => manejarRespuesta("a")), // Boton a
+                new BotonUI(Colors.blueAccent, new Text(_preguntaActual.darOpciones[1],style: TextStyle(fontSize: 50)),() => manejarRespuesta("b")), // Boton b
+                 ]
+              )
+            ),
+            
+           new PreguntaUI(new Text(textoDeLaPregunta)), // Pregunta
+
+            Expanded(
+              child: new Row(
+                 children: <Widget>[
+                new BotonUI(Colors.red, new Text(_preguntaActual.darOpciones[2],style: TextStyle(fontSize: 50)),() => manejarRespuesta("c")), // Boton c
+                new BotonUI(Colors.lime, new Text(_preguntaActual.darOpciones[3],style: TextStyle(fontSize: 50)),() => manejarRespuesta("d")), // Boton b
+                 ]
+              )
+            ),
+          ],
+        ),
+        
         overlayVisible == true ? new OverlayCorectoIncorrecto(
           esCorrecto, 
           () {
--- lib/utils/pregunta.dart
+++ lib/utils/pregunta.dart
@@ -1,3 +1,10 @@
+ 
+ 
+ enum TipoDePregunta {
+    vOF, multiple, abierta
+  }
+
+
 class Pregunta{
 
   //----------------------------------------------
@@ -7,17 +14,31 @@ class Pregunta{
    /// Atributo donde se guarda la pregunta
    String _pregunta;
    /// Atributo donde se guarda si la respuesta fue correcta o no
-   bool _respuesta;
+   String _respuesta;
+   /// Atributo que representa el tipo de pregunta
+   TipoDePregunta _tipoDePregunta;
+    /// Atributo que contiene una lista de opciones para Multiple
+  List <String> _opciones;
 
   //----------------------------------------------
-  // CONSTRUCTOR
+  // CONSTRUCTORES
   //----------------------------------------------
 
-  /// Constructor que recibe una pregunta de tipo String y un boolean sobre si la respuesta es corrrecta o no
-  Pregunta(pPregunta, pRespuesta)
+  /// Constructor para VoF y Abierta que recibe una pregunta de tipo String, un String para la respuesta y un enum para el tipo de pregunta
+  Pregunta.vofYabierta(pPregunta, pRespuesta, pTipoDePregunta)
   {
     _pregunta = pPregunta;
     _respuesta = pRespuesta;
+    _tipoDePregunta = pTipoDePregunta;
+  }
+
+  /// Constructor para Multiple que recibe una pregunta de tipo String, un String para la respuesta, un enum para el tipo de pregunta y una lista de 4 opciones
+  Pregunta.multiple(pPregunta, pRespuesta, pTipoDePregunta, pOpciones)
+  {
+    _pregunta = pPregunta;
+    _respuesta = pRespuesta;
+    _tipoDePregunta = pTipoDePregunta;
+    _opciones = pOpciones;
   }
 
   //----------------------------------------------
@@ -28,7 +49,13 @@ class Pregunta{
   String get darPregunta => _pregunta;
 
   /// Metodo que retorna la respuesta 
-  bool get darRespuesta => _respuesta;
+  String get darRespuesta => _respuesta;
+
+  /// Metodo que returna el tipo de pregunta
+  TipoDePregunta get darTipoDePregunta => _tipoDePregunta;
+
+  /// Metofo que retorna las opciones
+  List <String> get darOpciones =>  _opciones;
 
 
 
