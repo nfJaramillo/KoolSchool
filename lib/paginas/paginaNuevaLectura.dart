1f1640e2165d8ae3a74a33733d0d1423db02e0e7
ed.negrete <ed.negrete@uniandes.edu.co>
2019-11-13 12:19:27 -0500

    Agregar

---
+++ lib/paginas/paginaNuevaLectura.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+import 'package:Kool_School/paginas/paginaLecturaPdf.dart';
+
+import 'package:Kool_School/paginas/paginaMenu.dart';
+import 'package:Kool_School/paginas/paginaQuiz.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+import 'package:Kool_School/UI/botonUI.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'paginaFilePicker.dart';
+
+class PaginaNuevaLectura extends StatefulWidget
+{
+    final List<String> cons;
+  
+    PaginaNuevaLectura({Key key, this.cons}): super(key: key);
+
+
+ 
+   PaginaNuevaLecturaEstado createState() => new PaginaNuevaLecturaEstado();
+
+}
+
+class PaginaNuevaLecturaEstado extends State<PaginaNuevaLectura>
+{
+
+   List<String> lista;
+
+  PaginaNuevaLecturaEstado (){
+    lista = [];
+    //lista = widget.cons;
+  }
+
+  moverPdf(int pNumeroPdf){
+ 
+      lista = widget.cons;
+  }
+
+ 
+  cargarPdf(int pNumeroPdf) async {
+    //darNumeroArchivos();
+    PDFDocument _documento = await PDFDocument.fromPath(widget.cons[pNumeroPdf]);
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) => new PaginaLecturaPdf(_documento,( () =>Navigator.of(context).push (new MaterialPageRoute(builder: (BuildContext context) => new PaginaQuiz(quiz[pNumeroPdf])))))));
+
+    
+
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+        backgroundColor: Colors.redAccent,
+        body: SingleChildScrollView(
+          
+          child: ListView(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  children: [
+                    //Padding(padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .02)),
+                    for(int i =0; i <widget.cons.length;i++)
+                      new BotonUI3(Colors.amber, new Text(widget.cons[i]), () => cargarPdf(i)),
+           
+                       Padding(
+              padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .015), // Añade espacio entre los textos
+            ),
+                   new BotonUI2(Colors.limeAccent, new Text("Cargar"), () => Navigator.of(context).push(new MaterialPageRoute( builder: (BuildContext context) => new FilePickerDemo()))),
+                        Padding(
+              padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .015), // Añade espacio entre los textos
+            ),
+                  ],
+                ),
+          ),
+        );
+  }
+
+}
--- lib/paginas/paginaPdf.dart
+++ lib/paginas/paginaPdf.dart
@@ -1,35 +1,50 @@
+
+
 import 'package:Kool_School/paginas/paginaLecturaPdf.dart';
+
+import 'package:Kool_School/paginas/paginaNuevaLectura.dart';
+
 import 'package:Kool_School/paginas/paginaMenu.dart';
 import 'package:Kool_School/paginas/paginaQuiz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 import 'package:Kool_School/UI/botonUI.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'paginaFilePicker.dart';
 
 class PaginaPdf extends StatefulWidget
 {
   @override
-  State createState  () => new PaginaPdfEstado();
+   PaginaPdfEstado createState() => new PaginaPdfEstado();
 
 }
 
 class PaginaPdfEstado extends State<PaginaPdf>
 {
 
+
   //----------------------------------------------
   // ATRIBUTOS
   //----------------------------------------------
 
-
+  
 
   /// Atributo que tendra al documento pdf
   PDFDocument _documento;
   /// Lista de las rutas de los pdf
-  List <String> _nomrbresPDF;
+  //List <String> _nomrbresPDF;
+  ///numero de arch
+  int numeroArchivos;
+  ///directorio
+  String lolo;  
 
-  List<String> _nombresConOrtografia = ["Historia Indígena (Español 1°)", "Las Lunas de Júpiter (Ciencias 4°)", "Las Vacunas (Ciencias 5°)", "Los Sentidos (Naturales 2°)"  ];
+  List <String> colecciones;
+
+  var ni;
+
+  
 
   //----------------------------------------------
   // CONSTRUCTOR
@@ -37,15 +52,20 @@ class PaginaPdfEstado extends State<PaginaPdf>
 
   /// Constructor que llama al metodo cargarPdf(). Esto para que solo se llame una vez cuando de crea esta clase
   PaginaPdfEstado()
-  {
-    _nomrbresPDF = ["assets/Historia Indigena (Español 1°).pdf",'assets/Las lunas de jupiter (Ciencias 4°).pdf','assets/Las vacunas (Ciencias 5°).pdf','assets/Los sentidos (Naturales 2°).pdf'];
-  }
-<<<<<<< HEAD
+   {
+    
 
 
-=======
+    lolo = "";
+    colecciones = [];
+
+    
+    //_nomrbresPDF = [];
+
+
+    
+    }
   
->>>>>>> 049ceef4c0e69a73edca8c618ce55f870985d53f
 
 
   //----------------------------------------------
@@ -55,50 +75,52 @@ class PaginaPdfEstado extends State<PaginaPdf>
 
   /// Metodo que carga el archivo desde un asset y mantiene el thread ahi mientras lo hace. En ese tiempo aparece el circulo de carga azul en la pantalla
   cargarPdf(int pNumeroPdf) async {
-<<<<<<< HEAD
-
-    _documento = await PDFDocument.fromAsset(_nomrbresPDF[pNumeroPdf]);
-    Navigator.of(context).push(new MaterialPageRoute(builder: (context) => new PaginaLecturaPdf(_documento,( () =>Navigator.of(context).push (new MaterialPageRoute(builder: (BuildContext context) => new PaginaQuiz(quiz[pNumeroPdf])))))));
-=======
-    
-     _documento = await PDFDocument.fromAsset(_nomrbresPDF[pNumeroPdf]);
+    //darNumeroArchivos();
+     _documento = await PDFDocument.fromAsset(colecciones[pNumeroPdf]);
      Navigator.of(context).push(new MaterialPageRoute(builder: (context) => new PaginaLecturaPdf(_documento,( () =>Navigator.of(context).push (new MaterialPageRoute(builder: (BuildContext context) => new PaginaQuiz(quiz[pNumeroPdf])))))));
->>>>>>> 049ceef4c0e69a73edca8c618ce55f870985d53f
   }
 
+  darNumeroArchivos() async {
+      var ne = await getApplicationDocumentsDirectory();
+      int val = await ne.list().length;
+      numeroArchivos = val;
+      print(numeroArchivos);
+  }
+
+  listarPDFs() async {
+      
+      var ne = await getApplicationDocumentsDirectory();
+      
+      int num = await ne.list().length;
+
+      for(int i = 0; i<num; i++){
+
+        var nuno = await ne.list().elementAt(i);
+        
+        lolo = nuno.path;
+        //print(lolo);
+
+        if(lolo.endsWith(".pdf")){ 
+          String nombre =lolo.split('/').last;
+          //print(nombre);
+          colecciones.add(nombre);
+        }
+
+      //_nomrbresPDF.add("value");
+      }
+      //print(lista.length);
+      //int ni = await nero.length;
+      //print(ni);
+
+      Navigator.of(context).push(new MaterialPageRoute(builder: (context) => new PaginaNuevaLectura(cons: colecciones)));
+      }
+
+      
   /// Metodo que dibujara el visor de pdf junto con un titilo y ayuda de la libreria
   @override
   Widget build(BuildContext context) {
-    List<int> valuesForColor = [600, 700, 800, 900];
-<<<<<<< HEAD
-    return Scaffold(
-      backgroundColor: Colors.purple[600],
-      body: SingleChildScrollView(
-
-        child: ListView(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          shrinkWrap: true,
-          physics: ClampingScrollPhysics(),
-          children: [
-
-            for(int i =0; i<_nomrbresPDF.length;i++)
-            //Padding(padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .02))
-              new BotonUI3(Colors.amber[valuesForColor[i%4]], new Text(_nombresConOrtografia[i]), () => cargarPdf(i)),
-
-            Padding(
-              padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .015), // Añade espacio entre los textos
-            ),
-            new BotonUI2(Colors.purple[900], new Text("Otros"), () => Navigator.of(context).push(new MaterialPageRoute( builder: (BuildContext context) => new FilePickerDemo()))),
-            Padding(
-              padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .015), // Añade espacio entre los textos
-            ),
-          ],
-        ),
-      ),
-    );
-=======
    return Scaffold(
-        backgroundColor: Colors.purple[600],
+        backgroundColor: Colors.redAccent,
         body: SingleChildScrollView(
           
           child: ListView(
@@ -106,15 +128,14 @@ class PaginaPdfEstado extends State<PaginaPdf>
                   shrinkWrap: true,
                   physics: ClampingScrollPhysics(),
                   children: [
-
-                    for(int i =0; i<_nomrbresPDF.length;i++)
-                      //Padding(padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .02))
-                       new BotonUI3(Colors.amber[valuesForColor[i%4]], new Text(_nombresConOrtografia[i]), () => cargarPdf(i)),
+                    //Padding(padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .02)),
+                    //for(int i =0; i <colecciones.length;i++)
+                      new BotonUI3(Colors.amber, new Text("Lectura"), () => listarPDFs()),
            
                        Padding(
               padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .015), // Añade espacio entre los textos
             ),
-                   new BotonUI2(Colors.purple[900], new Text("Otros"), () => Navigator.of(context).push(new MaterialPageRoute( builder: (BuildContext context) => new FilePickerDemo()))),
+                   new BotonUI2(Colors.limeAccent, new Text("Cargar"), () => Navigator.of(context).push(new MaterialPageRoute( builder: (BuildContext context) => new FilePickerDemo()))),
                         Padding(
               padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .015), // Añade espacio entre los textos
             ),
@@ -122,9 +143,6 @@ class PaginaPdfEstado extends State<PaginaPdf>
                 ),
           ),
         );
->>>>>>> 049ceef4c0e69a73edca8c618ce55f870985d53f
   }
-
-}
-
+  
 }
